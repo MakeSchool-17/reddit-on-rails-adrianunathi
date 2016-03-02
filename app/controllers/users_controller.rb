9068cf30f77945faed8a2a4450f86ccc305c0c91
Unathi Chonco <choncou@gmail.com>
2016-03-01 22:43:40 -0800

    Add Scaffolding for users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,44 @@
 class UsersController < ApplicationController
 
   def new
+    @user = User.new
   end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      #It was valid and saved
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def destroy
+    User.find(params[:id]).destroy
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      #Show flash that it succeeded or something
+      redirect_to @user
+    else
+      # Show edit page ?
+    end
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:username, :email, :password, :password_confirmation)
+    end
 
 end
