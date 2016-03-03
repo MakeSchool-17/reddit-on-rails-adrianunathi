9beea19e80f12f84bc86c627e9bed4786b711618
Adrian Wisaksana <dev@beingadrian.com>
2016-03-03 11:38:23 -0800

    Add Subscription node

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,8 +8,6 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       #It was valid and saved
-    else
-      render 'new'
     end
   end
 
@@ -29,7 +27,6 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     if @user.update_attributes(user_params)
       #Show flash that it succeeded or something
-      redirect_to @user
     else
       # Show edit page ?
     end
